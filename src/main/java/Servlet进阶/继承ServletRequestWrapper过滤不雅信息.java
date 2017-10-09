68d90516444f22e38e5ff6f6b177e03fcf9759cd
shujinzhe <946636673@qq.com>
2017-10-09 09:27:26 +0800

    2017-10-09

---
+++ src/main/java/Servlet进阶/继承ServletRequestWrapper过滤不雅信息.java
@@ -0,0 +1,24 @@
+package Servlet进阶;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class 继承ServletRequestWrapper过滤不雅信息  extends HttpServletRequestWrapper{
+
+	public 继承ServletRequestWrapper过滤不雅信息(HttpServletRequest request) {
+		super(request);
+	}
+
+	/**
+	 * 这里使用了装饰器模式，配合Filter使用，可以实现对getParameter的过滤
+	 */
+	public String getParameter(String name) {
+		// TODO Auto-generated method stub
+		String value = super.getParameter(name);
+		if(value!=null && value.contains("bad")){
+			value.replaceAll("bad", "***");
+		}
+		return value;
+	}
+
+}
